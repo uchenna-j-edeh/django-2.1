6b6683659c9d06fa42035465a5f0394a6190d320
Uchenna Edeh <write2uche@gmail.com>
2018-11-01 07:58:13 -0500

    chapter 10 completed...

--- mfdw_root/pages/urls.py
+++ mfdw_root/pages/urls.py
@@ -1,9 +1,11 @@
-from django.conf.urls import url
 from django.urls import path
 from . import views
 
 urlpatterns = [
-#    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     path('', views.index, {'pagename': ''}, name='home'),
+    path('contact', views.contact, name='contact'),
     path('<str:pagename>', views.index, name='index'),
 ]
+
+
--- mfdw_root/pages/views.py
+++ mfdw_root/pages/views.py
@@ -1,22 +1,42 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.core.mail   import send_mail, get_connection
 
 from . models import Page
-# from django.http import HttpResponse
+from .forms import ContactForm
 
 def index(request, pagename):
-#    return HttpResponse("<h1>The Edeh Bookstore Homepage</h1>")
     pagename = '/' + pagename
-    #print(pagename)
-    #print(pg)
     pg = get_object_or_404(Page, permalink=pagename)
-
-#    pg = Page.objects.get(permalink=pagename)
     context = {
         'title': pg.title,
         'content': pg.bodytext,
         'last_updated': pg.update_date,
-        'page_list': Page.objects.all()
+        'page_list': Page.objects.all(),
     }
     # assert False
-#    print(context)
     return render(request, 'pages/page.html', context)
+
+def contact(request):
+    submitted = False
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            # assert False
+            con = get_connection('django.core.mail.backends.console.EmailBackend')
+            send_mail(
+                cd['subject'],
+                cd['message'],
+                cd.get('email', 'noreply@example.com'),
+                ['siteowner@example.com'],
+                connection=con
+            )
+            return HttpResponseRedirect('/contact?submitted=True')
+    else:
+        form = ContactForm()
+        if 'submitted' in request.GET:
+            submitted = True
+
+    return render(request, 'pages/contact.html', {'form': form, 'page_list': Page.objects.all(), 'submitted': submitted})
+
