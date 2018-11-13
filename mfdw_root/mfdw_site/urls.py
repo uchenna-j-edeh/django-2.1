014d920dd757d3be6695c2020486d876161883b1
Uchenna Edeh <write2uche@gmail.com>
2018-11-13 07:50:19 -0600

    up to chapter...

--- mfdw_root/mfdw_site/urls.py
+++ mfdw_root/mfdw_site/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+#from django.views.generic.base import TemplateView
+from django.views.generic import TemplateView
+from quotes.views import Register
+
 urlpatterns = [
+#    path('testpage', TemplateView.as_view(template_name='pages/page.html')),
+    path('register/success/', TemplateView.as_view(template_name='registration/success.html'), name='register-success'),
+    path('register/', Register.as_view(), name='register'),
     path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls')),
+    path('quote/', include('quotes.urls')),
     path('', include('pages.urls')),
 ]
+
